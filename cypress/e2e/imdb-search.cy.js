9347c8364bd88b149273b8e9bcb2cd26018301ba
botirbek.kulzhanov <botirbek.kulzhanov@fabrique.studio>
2022-11-14 18:39:16 +0300

    Added some comments

--- cypress/e2e/imdb-search.cy.js
+++ cypress/e2e/imdb-search.cy.js
@@ -35,6 +35,7 @@ describe('Search functionality test', () => {
   })
 
   it('Should search by Category', () => {
+    // Explanation of Custom commands can be found in cypress\support\commands.js
     cy.searchBy('All', 'Titles')
     cy.search('Bullet Train')
 
@@ -51,39 +52,61 @@ describe('Search functionality test', () => {
     cy.search('Action')
   })
 
-  it('Advanced Search shoul be available', () => {
+  it('Advanced Search should be available', () => {
+    // Cypress xpath devDependency to help locate web-elements easily 
     cy.xpath(`//div[text()="Keywords"]`).click()
+    // Checks Advanced Search page is loaded successfully
     cy.intercept('GET', '/search').as('getSearchPage')
     cy.contains('Advanced Search').click()
     cy.wait('@getSearchPage').its('response.statusCode').should('eq', 200)
-    
+    // Checks whether it is in the rigth url address
     cy.url().should('contain', '/search')
-
+    // To replace explicit wait good practice to check visibility of elements instead      
     cy.get('#header > h1').should('be.visible')
     cy.get('#header > h3').should('be.visible')
-
     cy.get('.imdb-search-gateway__options').should('be.visible')
 
+    /* Advanced Search by title intercepts the
+    * /search/title/ request and checks visibility of main div
+    * if it fails user further can not interact and do advanced search
+    */ 
     cy.intercept('GET', '/search/title/').as('getTitleSearch')
+    // Custom clickLink command
     cy.clickLink('Advanced Title Search')
     cy.url().should('contain', '/search/title/')
     cy.wait('@getTitleSearch').its('response.statusCode').should('eq', 200)
-
+    cy.get('#main').should('be.visible')
+   
+    /* Returns to the 4 Ways to Search, 
+    * often return back function does not work properly in web-apps.
+    * For instance, user should be able to return back to previous page without any error
+    * and clicking only once rather than clicking 2-3 times
+    */
     cy.go('back')
     cy.get('.imdb-search-gateway__options').should('be.visible')
-
+    
+    /* Advanced Name Search intercepts the
+    * /search/name/ request and checks visibility of main div
+    * if it fails user further can not interact and do advanced search
+    */ 
     cy.intercept('GET', '/search/name/').as('getNameSearch')
     cy.clickLink('Advanced Name Search')
     cy.url().should('contain', '/search/name/')
     cy.wait('@getNameSearch').its('response.statusCode').should('eq', 200)
+    cy.get('#main').should('be.visible')
 
     cy.go('back')
     cy.get('.imdb-search-gateway__options').should('be.visible')
 
+    /* Advanced Search Collaborations intercepts the
+    * /search/common/request and checks visibility of main div
+    * if it fails user further can not interact and do advanced search
+    */ 
     cy.intercept('GET', '/search/common/').as('getCollabSearch')
     cy.clickLink('Search Collaborations')
     cy.url().should('contain', '/search/common/')
     cy.wait('@getCollabSearch').its('response.statusCode').should('eq', 200)
+    cy.get('#main').should('be.visible')
 
     cy.go('back')
     cy.get('.imdb-search-gateway__options').should('be.visible')
--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -24,12 +24,23 @@
 // -- This will overwrite an existing command --
 // Cypress.Commands.overwrite('visit', (originalFn, url, options) => { ... })
 
+/*
+* Custom command to search by category
+* accepts two arguments currentCategory and nextCategory
+* In test automation DRY concept is relevant and adviced to use
+*/
+
 Cypress.Commands.add('searchBy', (currentCategory, nextCategory) => {
     cy.xpath(`//div[text()="${currentCategory}"]`).click()
     cy.get('[data-menu-id="navbar-search-category-select"]').should('be.visible')
     cy.xpath(`//li/span[text()="${nextCategory}"]`).should('be.visible').click()
 })
 
+/*
+* Custom search command
+* accepts search term and waits for api response and checks the statusCode
+* It is a good practice to to use implicit wait as explicit to simulate real world scenarios
+*/
 Cypress.Commands.add('search', (searchTerm) => {
     cy.intercept('GET', '/find*').as('getResults')
     
@@ -38,6 +49,10 @@ Cypress.Commands.add('search', (searchTerm) => {
     cy.wait('@getResults').its('response.statusCode').should('eq', 200)
 })
 
+/*
+* Custom command to click links
+* which can be reused
+*/
 Cypress.Commands.add('clickLink', (label) => {
     cy.get('a').contains(label).click()
 })
